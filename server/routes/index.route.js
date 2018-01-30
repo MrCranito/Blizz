12550207b48047acd237545781f78c7112a318af
MedRachid <32295843+MedRachid@users.noreply.github.com>
2018-01-30 18:02:23 +0100

    Fixing routes & Adding Logs

---
+++ server/routes/index.route.js
@@ -0,0 +1,14 @@
+var express = require('express');
+module.exports.publicRoutes = function(){
+	var router = express.Router();
+
+	return router;
+}
+
+module.exports.privateRoutes= function(){
+
+	var router = express.Router();
+
+	return router;
+
+}
